e373502f65b729ddf44cf42a64567de04cb9d163
Italo Armenti <italo.armenti@gmail.com>
2026-02-06 13:25:57 +0200

    upgrade gradle and libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,7 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "2.3.0"
     application
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
 group = "me.italoarmenti"
@@ -19,8 +17,8 @@ dependencies {
     implementation("io.ktor:ktor-client-apache:1.6.7")
     implementation("io.ktor:ktor-client-gson:1.6.7")
     implementation("com.xenomachina:kotlin-argparser:2.0.7")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.0")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.6.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:2.3.0")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.3.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
@@ -29,21 +27,16 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(8)
 }
 
 // create executable with :./gradlew clean installShadowDist
-tasks {
-    named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-        archiveBaseName.set("staking_calculator")
-        mergeServiceFiles()
-    }
+tasks.shadowJar {
+    archiveBaseName.set("staking_calculator")
+    mergeServiceFiles()
 }
 
 application {
     mainClass.set("MainKt")
 }
-
-// Required by the 'shadowJar' task
-project.setProperty("mainClassName", "MainKt")
